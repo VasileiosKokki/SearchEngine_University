72a7012d3ab68b924d5a8a58f7a93b2221263981
VasileiosKokki <vasilischool@gmail.com>
2025-05-18 23:26:51 +0300

    sentence embeddings

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import torch
+
 from functions import *
 
 def startup():
@@ -6,40 +8,97 @@ def startup():
 
     stopwords = load_stopwords()
 
-    # one time only, then we simply open
-    # save_bm25_representation()
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available() and torch.backends.mps.is_built():
+        device = "mps"
+    else:
+        device = "cpu"
 
-    # takes a lot of time
+    print(f"Using device: {device}")
+    model_name = "all-MiniLM-L6-v2"
+    model = SentenceTransformer(model_name, device=device)
+    docs_metadata = load_all_docs_metadata('wiki_movie_plots_deduped.csv')
+
+
+    if not os.path.exists("corpus_index.pkl") or not os.path.exists("idf_values.pkl"):
+        save_bm25_representation(stopwords, docs_metadata)
+
+    if not os.path.exists("sentence_embeddings.pkl"):
+        save_sentence_embeddings(model, docs_metadata)
+
+
     with open("corpus_index.pkl", "rb") as file:
         corpus_index = pickle.load(file)
 
     with open("idf_values.pkl", "rb") as file:
         idf_values = pickle.load(file)
 
+
+    with open("sentence_embeddings.pkl", "rb") as file:
+        sentence_embeddings = pickle.load(file)
+
     # ---------- Queries ----------
     # # ACTION: open the queries file and read three queries
     queries = {}
-    with open('../dev.titles.queries', 'r', encoding='utf-8') as f:
-        for line in f:
-            fields = line.strip().split('\t')
-            q_id = fields[0].strip()
-            q_text = fields[1].strip()
+    with open('dev.titles.queries', 'r', encoding='utf-8') as f:
+        for i, line in enumerate(f):
+            q_id = f"Q{i+1}"
+            q_text = line.strip()
+            queries[q_id] = q_text
 
-            if q_id in ['PLAIN-1', 'PLAIN-11', 'PLAIN-111']:
-                queries[q_id] = q_text
-
-    # ---------- 2) Implement an indexing mechanism ----------
-
-    # trie = build_trie_index(tfidf_representation)
 
     # ---------- 3) Implement the mechanism for retrieving and calibrating the results ----------
 
-    doc_ids = list(corpus_index.keys())
+    doc_ids = list(docs_metadata.keys())
+
     avgdl = compute_avgdl(corpus_index)
 
-    for q_id, q_text in queries.items():
-        ranked_docs = top_n_similar_documents(q_text, stopwords, doc_ids, corpus_index, idf_values, avgdl)
-        print(q_id, queries[q_id], ranked_docs)
+    top10_results_MiniLM = get_top10_results_per_query(
+                                queries=queries,
+                                doc_ids=doc_ids,
+                                sentence_embeddings=sentence_embeddings,
+                                model=model,
+                                model_name=model_name
+                            )
 
-    # return vectorizer, tfidf_representation, trie, vocabulary
-    return corpus_index, idf_values, doc_ids, stopwords, avgdl
+    top10_results_BM25 = get_top10_results_per_query(
+                                queries=queries,
+                                doc_ids=doc_ids,
+                                model_name="bm25",
+                                corpus_index=corpus_index,
+                                stopwords=stopwords,
+                                idf_values=idf_values,
+                                avgdl=avgdl
+                            )
+
+    path1 = f'dev.3-2-1_bm25.qrel'
+    path2 = f'dev.3-2-1_{model_name}.qrel'
+    if not os.path.exists("golden_answers"):
+        merge_and_filter_golden_answers(path1, path2)
+
+    golden_answers = {}
+    # we assume that all the retrieved are somewhat relevant
+    with open("golden_answers", 'r', encoding='utf-8') as f:
+        for line in f:
+            fields = line.strip().split()
+            # Assuming the ID is the first field, title is third and abstract is the fourth field
+            q_id = fields[0].strip()
+            doc_id = fields[1].strip()
+            score = int(fields[2].strip())
+            if q_id in golden_answers.keys():
+                golden_answers[q_id].append((doc_id, score))
+            else:
+                golden_answers[q_id]=[]
+                golden_answers[q_id].append((doc_id, score))
+
+
+
+    evaluate_topk_results(golden_answers, top10_results_MiniLM, model_name=model_name)
+    evaluate_topk_results(golden_answers, top10_results_BM25, model_name="bm25")
+
+
+
+
+
+    return corpus_index, idf_values, doc_ids, stopwords, avgdl, sentence_embeddings, model, docs_metadata
--- server.py
+++ server.py
@@ -3,15 +3,14 @@ import time
 
 from flask import Flask, request, jsonify, send_from_directory
 
-from functions import load_all_docs_metadata, top_n_similar_documents
+from functions import load_all_docs_metadata, bm25, semantic_similarity
 from main import startup
 
 app = Flask(__name__)
 
 with app.app_context():
     # vectorizer, tfidf_representation, trie, vocabulary = startup()  # Call the startup function to initialize the global variables
-    corpus_index, idf_values, doc_ids, stopwords, avgdl = startup()
-    docs_metadata = load_all_docs_metadata('doc_dump.txt')
+    corpus_index, idf_values, doc_ids, stopwords, avgdl, sentence_embeddings, transformer_model, docs_metadata = startup()
     print("Flask app startup complete.")  # Debugging line to confirm the startup
 
 
@@ -22,12 +21,19 @@ def home():
 def search():
     start_time = time.time()  # Start timing
     user_query = request.args.get('query')  # Get ?query= from URL
+    search_by = request.args.get('searchBy')
+    use_semantic_search = request.args.get('useSemanticSearch') == 'true'
 
     if not user_query:
         return jsonify({'error': 'No query provided'}), 400
 
     # Tokenize the query
-    ranked_docs = top_n_similar_documents(user_query, stopwords, doc_ids, corpus_index, idf_values, avgdl)
+    if use_semantic_search:
+        scores = semantic_similarity(doc_ids, user_query, sentence_embeddings[search_by], transformer_model)
+    else:
+        scores = bm25(corpus_index[search_by], user_query, stopwords, idf_values[search_by], avgdl[search_by])
+
+    ranked_docs = sorted(zip(doc_ids, scores), key=lambda x: x[1], reverse=True)[:10]
 
     # Format the response with metadata
     results = []
@@ -36,9 +42,14 @@ def search():
         if metadata:
             results.append({
                 'doc_id': doc_id,
+                'release_year': metadata['release_year'],
                 'title': metadata['title'],
-                'abstract': metadata['abstract'],
-                'url': metadata['url']
+                'origin': metadata['origin'],
+                'director': metadata['director'],
+                'cast': metadata['cast'],
+                'genre': metadata['genre'],
+                'url': metadata['url'],
+                'plot': metadata['plot'],
             })
 
 
@@ -46,7 +57,7 @@ def search():
     duration = end_time - start_time
     print(duration)
     # Return the results
-    return jsonify({'results': results})
+    return jsonify({'results': results, 'time_needed': duration})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5001)
